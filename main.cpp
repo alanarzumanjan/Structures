948574a972993b671ce0fc47453b7b2642d5bcf6
Alans Arzumanjans <117813243+alanarzumanjan@users.noreply.github.com>
2024-03-21 13:03:07 +0000

    mark

--- main.cpp
+++ main.cpp
@@ -37,7 +37,6 @@ public:
 
     void ListDisplay() {
         Node* current = head;
-
         while (current != nullptr) {
             cout << current->data << " ";
             current = current->next;
@@ -49,28 +48,78 @@ public:
         Node* prev = nullptr;
         Node* current = head;
         Node* next = nullptr;
-
         while (current != nullptr) {
             next = current->next;
             current->next = prev;
             prev = current;
             current = next;
         }
-        
         head = prev;
     }
+
+    void SortAddr() {
+        if (head == nullptr || head->next == nullptr)
+        return;
+
+        bool swapped;
+        Node* temp;
+
+        do {
+            swapped = false;
+            Node* current = head;
+            Node* prev = nullptr;
+
+            while (current->next != nullptr) {
+                if (current > current->next) { 
+                    if (prev != nullptr) {
+                        prev->next = current->next;
+                    } else {
+                        head = current->next;
+                    }
+                    
+                    temp = current->next->next;
+                    current->next->next = current;
+                    current->next = temp;
+
+                    swapped = true;
+                }
+
+                prev = current;
+                current = current->next;
+            }
+        } while (swapped);
+    }
+
 };
+
+void PrintAddresses(const LinkedList& list) {
+    Node* current = list.head;
+    while (current != nullptr){
+        cout << current << " ";
+        current = current->next;
+    }
+    cout << endl;
+}
 
 int main() {
     LinkedList list;
     list.append(1);
     list.append(2);
     list.append(3);
-    list.append(0);
-
+    
+    cout << "List: ";
     list.ListDisplay();
+    
+    cout << "Reverse List: ";
     list.ReverseList();
     list.ListDisplay();
 
+    cout << "Addr:" << endl;
+    PrintAddresses(list);
+    
+    cout << "Sorting addr:" << endl;
+    list.SortAddr();
+    PrintAddresses(list);
+    
     return 0;
 }
