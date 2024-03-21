82628e06c9ee0805db31963d58f9ad43b3392291
Alans Arzumanjans <117813243+alanarzumanjan@users.noreply.github.com>
2024-03-21 11:10:30 +0000

    Mommy Markus

---
+++ Makefile
@@ -0,0 +1,23 @@
+TARGET = main
+SRC_FILE = $(TARGET).cpp
+
+CC = g++
+CFLAGS = -lm -std=c++14
+
+all: clean build run
+
+build:
+	$(CC) $(SRC_FILE) -o $(TARGET) $(CFLAGS)
+
+run:
+	./$(TARGET)
+
+clean:
+	rm -f $(TARGET) *.o
+	
+push:
+	git add .
+	git commit -a -m "Mommy Markus"
+	git pull
+	git rebase
+	git push
---
+++ main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Node {
+public: 
+    int data;
+    Node* next;
+
+    Node(int data) {
+        this->data = data;
+        this->next = nullptr;
+    }
+};
+
+class LinkedList {
+public:
+    Node* head;
+
+    LinkedList() {
+        this->head = nullptr;
+    }
+
+    void append(int data) {
+        Node* newNode = new Node(data);
+        if (head == nullptr) {
+            head = newNode;
+            return;
+        }
+        Node* current = head;
+        while (current->next != nullptr) {
+            current = current->next;
+        }
+        current->next = newNode;
+    }
+
+    void ListDisplay() {
+        Node* current = head;
+
+        while (current != nullptr) {
+            cout << current->data << " ";
+            current = current->next;
+        }
+        cout << endl;
+    }
+    
+    void ReverseList() {
+        Node* prev = nullptr;
+        Node* current = head;
+        Node* next = nullptr;
+
+        while (current != nullptr) {
+            next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
+        }
+        
+        head = prev;
+    }
+};
+
+int main() {
+    LinkedList list;
+    list.append(1);
+    list.append(2);
+    list.append(3);
+    list.append(0);
+
+    list.ListDisplay();
+    list.ReverseList();
+    list.ListDisplay();
+
+    return 0;
+}
